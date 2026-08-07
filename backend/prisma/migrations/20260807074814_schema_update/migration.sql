c922cf7c4ebc95da814252663669be5f6d64af75
Mati Melkamu <matimelkamu15@gmail.com>
2026-08-07 11:36:12 +0300

    Docs

---
+++ backend/prisma/migrations/20260807074814_schema_update/migration.sql
@@ -0,0 +1,45 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Category` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `PostTag` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Tag` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_categoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "PostTag" DROP CONSTRAINT "PostTag_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "PostTag" DROP CONSTRAINT "PostTag_tagId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "posts" DROP CONSTRAINT "posts_author_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "posts" DROP CONSTRAINT "posts_category_id_fkey";
+
+-- AlterTable
+ALTER TABLE "posts" ALTER COLUMN "category_id" DROP NOT NULL,
+ALTER COLUMN "content" DROP NOT NULL;
+
+-- DropTable
+DROP TABLE "Category";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- DropTable
+DROP TABLE "PostTag";
+
+-- DropTable
+DROP TABLE "Tag";
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
